3b250475a47d66b5594a8f9c2b2c0ba6386b085c
Kristian Serrano <kristianserrano@gmail.com>
2023-12-11 15:11:48 -0500

    Fixes empty string for value reset

--- modules/gamemaster-title-editor.js
+++ modules/gamemaster-title-editor.js
@@ -1,4 +1,5 @@
 const MODULE_ID = 'gamemaster-title-editor';
+const SOURCE = { title: '' };
 
 Hooks.on('init', () => {
 
@@ -12,21 +13,40 @@ Hooks.on('init', () => {
         submitOnChange: false,
         requiresReload: false,
         onChange: async value => {
-            game.i18n.translations.GM = value;
-            game.i18n.translations.USER.GM = value;
+            if (value === '') {
+                const sourceTitle = game.settings.get(MODULE_ID, "sourceTitle");
+                game.i18n.translations.USER.GM = sourceTitle;
+                game.i18n.translations.GM = sourceTitle;
+            } else {
+                game.i18n.translations.USER.GM = value;
+                game.i18n.translations.GM = value;
+            }
+
             const playersList = game.users.apps.find(app => app.id === 'players');
             playersList.render();
-            game.webrtc.render()
+            game.webrtc.render();
         }
     });
+
+    game.settings.register(MODULE_ID, 'sourceTitle', {
+        scope: 'world',
+        config: false,
+        type: String,
+        default: '',
+    });
+
 });
 
 Hooks.on('setup', async () => {
+    SOURCE.title = game.i18n.translations.USER.GM;
     const title = game.settings.get(MODULE_ID, "customGamemasterTitle");
 
     if (title) {
-        game.i18n.translations.GM = title;
         game.i18n.translations.USER.GM = title;
+        game.i18n.translations.GM = title;
     }
+});
 
+Hooks.on('ready', async () => {
+    await game.settings.set(MODULE_ID, "sourceTitle", SOURCE.title);
 });
