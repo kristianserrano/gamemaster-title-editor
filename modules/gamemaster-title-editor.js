f7f07e0a670e3450b7c227f101ac1244b9a241a4
Kristian Serrano <kristianserrano@gmail.com>
2025-03-30 18:29:08 -0400

    Fixes attempt error with attempting to set world setting during setup

--- modules/gamemaster-title-editor.js
+++ modules/gamemaster-title-editor.js
@@ -1,7 +1,6 @@
 const MODULE_ID = 'gamemaster-title-editor';
 
 Hooks.on('init', () => {
-
     game.settings.register(MODULE_ID, 'customGamemasterTitle', {
         name: game.i18n.localize('GameMasterTitleEditor.CustomGamemasterTitle.Name'),
         hint: game.i18n.localize('GameMasterTitleEditor.CustomGamemasterTitle.Hint'),
@@ -13,30 +12,17 @@ Hooks.on('init', () => {
         requiresReload: false,
         onChange: async (value) => {
             const name = value.trim();
-
-            if (name === '') {
-                const sourceTitle = game.settings.get(MODULE_ID, "sourceTitle");
-                game.i18n.translations.USER.GM = sourceTitle;
-            } else {
-                game.i18n.translations.USER.GM = name;
-            }
-
+            game.i18n.translations.USER.GM = name.length ? name : CONFIG.CustomGMTitle.coreDefault;
             await foundry.applications.instances.get('players')?.render();
             await foundry.applications.instances.get("camera-views").render();
         }
     });
-
-    game.settings.register(MODULE_ID, 'sourceTitle', {
-        scope: 'world',
-        config: false,
-        type: String,
-        default: '',
-    });
-
 });
 
 Hooks.on('setup', async () => {
-    game.settings.set(MODULE_ID, 'sourceTitle', game.i18n.translations.USER.GM);
+    CONFIG.CustomGMTitle = {
+        coreDefault: game.i18n.translations.USER.GM
+    }
     const customTitle = game.settings.get(MODULE_ID, "customGamemasterTitle").trim();
-    game.i18n.translations.USER.GM = customTitle.length ? customTitle : game.i18n.translations.USER.GM;
+    game.i18n.translations.USER.GM = customTitle.length ? customTitle : CONFIG.CustomGMTitle.coreDefault;
 });
